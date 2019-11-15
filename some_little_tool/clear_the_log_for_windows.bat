9a0f911e4070fd0e689138c64b86b297c13c2fd9
marko1616 <45327989+marko1616@users.noreply.github.com>
2019-11-15 16:50:10 +0800

    Create clear_the_log_for_windows.bat

---
+++ some_little_tool/clear_the_log_for_windows.bat
@@ -0,0 +1,6 @@
+@echo off
+color 0a
+DEL C:\Windows\System32\LogFiles\*.* /F /S /Q
+DEL C:\Windows\System32\config\*.EVT /F /S /Q
+DEL C:\Windows\System32\*.log /F /S /Q
+DEL C:\Windows\System32\*.txt /F /S
